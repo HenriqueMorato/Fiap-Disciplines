2eac0fc02dcfa1e2fd032ed2b70362ed234ac08a
HenriqueMorato <riquemorato@gmail.com>
2017-01-19 11:30:43 -0200

    initial commit

---
+++ DisciplinesFiap/DisciplinesFiapPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace DisciplinesFiap
+{
+	public partial class DisciplinesFiapPage : ContentPage
+	{
+		public DisciplinesFiapPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
